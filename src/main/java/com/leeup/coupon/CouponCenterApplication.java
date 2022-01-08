7e00237808977e56f5458fc420e50e90a86f5c3c
李闯 <lichuang.lc@elecwatt.com>
2022-01-08 15:59:07 +0800

    feat(初始化): 项目基础结构

---
+++ src/main/java/com/leeup/coupon/CouponCenterApplication.java
@@ -0,0 +1,13 @@
+package com.leeup.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leeup/coupon/CouponCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leeup.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
